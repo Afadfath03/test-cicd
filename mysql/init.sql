9f64d12ac7b5792cf48ede2d1da2a4e5035b362c
Afad Fath Musyarof Halim <afadfath03@gmail.com>
2025-05-19 01:41:26 +0700

    Refactor CI/CD pipeline and initialization script for clarity and efficiency

--- mysql/init.sql
+++ mysql/init.sql
@@ -1,10 +1,9 @@
-CREATE DATABASE IF NOT EXISTS db_admin_devel;
 USE db_admin_devel;
 
 CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
-    nim VARCHAR(20) NOT NULL, Sign in to enable AI completions, or disable inline completions in Settings (DBCode > AI).
+    nim VARCHAR(20) NOT NULL
 );
 
 INSERT INTO users (name, nim) VALUES
